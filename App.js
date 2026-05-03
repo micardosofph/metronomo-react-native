19a7fa6c7e7a24a961c92ab4ea64a308afb845f2
micardosofph <micardosofph@gmail.com>
2026-05-03 01:12:52 -0300

    added metronome sounds

--- App.js
+++ App.js
@@ -1,7 +1,57 @@
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Text, ScrollView } from 'react-native';
 import { useAudioPlayer } from 'expo-audio';
 
 const audioSource = require('./assets/Hello.mp3');
+
+const metronomeSounds = [
+  {
+    name: "Click clássico",
+    beatHigh: require('./assets/metronomeClicks/flClickHi.wav'),
+    beatLow: require('./assets/metronomeClicks/flClickLo.wav')
+  },
+  {
+    name: "Beep",
+    beatHigh: require('./assets/metronomeClicks/beepHi.wav'),
+    beatLow: require('./assets/metronomeClicks/beepLo.wav')
+  },
+  {
+    name: "Palma",
+    beatHigh: require('./assets/metronomeClicks/clapHi.wav'),
+    beatLow: require('./assets/metronomeClicks/clapLo.wav')
+  },
+  {
+    name: "Bateria",
+    beatHigh: require('./assets/metronomeClicks/snareHi.wav'),
+    beatLow: require('./assets/metronomeClicks/snareLo.wav')
+  },
+  {
+    name: "Lata",
+    beatHigh: require('./assets/metronomeClicks/canHi.wav'),
+    beatLow: require('./assets/metronomeClicks/canLo.wav')
+  },
+];
+
+function CreateMetronomeBeatsElement({ som }) {
+  const playerHigh = useAudioPlayer(som.beatHigh);
+  const playerLow = useAudioPlayer(som.beatLow);
+
+  const playSound = (player) => {
+    if (player) {
+      player.seekTo(0);
+      player.play();
+    }
+  };
+
+  return (
+    <View style={styles.soundItem}>
+      <Text style={styles.soundName}>{som.name}</Text>
+      <View style={styles.buttonGroup}>
+        <Button title="Alto" onPress={() => playSound(playerHigh)} />
+        <Button title="Baixo" onPress={() => playSound(playerLow)} />
+      </View>
+    </View>
+  );
+}
 
 export default function App() {
   const player = useAudioPlayer(audioSource);
@@ -16,7 +66,18 @@ export default function App() {
           player.play();
         }}
       />
+
+      <Text>Teste de metronomo beats</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}>
+        {metronomeSounds.map((som) => (
+          <CreateMetronomeBeatsElement key={som.name} som={som} />
+        ))}
+      </ScrollView>
+
     </View>
+
+
   );
 }
 
@@ -26,5 +87,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 10,
+  },
+
+  scrollView: {
+
+  },
+
+  scrollContent: {
+
+  },
+
+  soundItem: {
+
+  },
+
+  soundName: {
+
+  },
+
+  buttonGroup: {
+
   },
 });
